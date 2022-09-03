b66a1a98aaf50a919717a26e30a0785048d47227
Nikolay <82240268+NikolayMurashkin@users.noreply.github.com>
2022-09-03 09:54:51 +0700

    Update PriorityButton.jsx

--- src/components/priorityButton/PriorityButton.jsx
+++ src/components/priorityButton/PriorityButton.jsx
@@ -1,16 +1,32 @@
+import classNames from "classnames";
 import './priorityButton.css';
 
-const PriorityButton = ({color, text, setPriority}) => {
-    return (
-        <input
-            type="button"
-            id='priority'
-            name='priority'
-            onClick={() => setPriority(text)}
-            className='button'
-            value={text}
-            style={{backgroundColor: `${color}`}}/>
+const PriorityButton = ({color, text, priority, handleClick}) => {
+    const isPrioritySelected = (value) => priority === value;
 
+    const labelClasses = classNames({
+        'label': true,
+        'low': text === 'Low',
+        'medium': text === 'Medium',
+        'high': text === 'High'
+    })
+    return (
+        <>
+            <input
+                type="radio"
+                checked={isPrioritySelected(text)}
+                id={text}
+                name='priority'
+                onChange={handleClick}
+                className='button'
+                value={text}
+
+            />
+            <label
+                style={{backgroundColor: `${color}`}}
+                htmlFor={text}
+                className={labelClasses}><span>{text}</span></label>
+        </>
     )
 }
 
